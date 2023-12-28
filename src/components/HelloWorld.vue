14a183466fff29c989ca8a06c366b3dbb5b4abb7
Ramiro Saenz <saenzramiro@gmail.com>
2023-12-28 17:20:28 -0300

    asd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps<{ msg: string }>();
 	<h1>{{ msg }}</h1>
 
 	<div class="card">
-		<button type="button" @click="counterStore.increment()">count is {{ counterStore.count }}</button>
+		<Button type="button" @click="counterStore.increment()">count is {{ counterStore.count }}</Button>
 		<p>
 			Edit
 			<code>components/HelloWorld.vue</code> to test HMR
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 
+import Button from 'primevue/button';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-green/theme.css';
+
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(PrimeVue);
 app.use(pinia);
+
+// eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
+app.component('Button', Button);
+
 app.mount('#app');
